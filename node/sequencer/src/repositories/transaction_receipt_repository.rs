9b6c40986c9b536da828f90e90d434f951ce63e4
Daniyar Itegulov <ditegulov@gmail.com>
2025-07-07 21:53:38 +1000

    test: write basic transfer and L1 deposit tests

--- node/sequencer/src/repositories/transaction_receipt_repository.rs
+++ node/sequencer/src/repositories/transaction_receipt_repository.rs
@@ -1,11 +1,16 @@
+use crate::CHAIN_ID;
 use alloy::consensus::transaction::TransactionInfo;
-use alloy::consensus::{Receipt, ReceiptEnvelope, ReceiptWithBloom, Transaction, TxType};
-use alloy::primitives::{Address, BlockHash};
+use alloy::consensus::{
+    Receipt, ReceiptEnvelope, ReceiptWithBloom, Signed, Transaction, TxLegacy, TxType,
+};
+use alloy::primitives::{Address, BlockHash, TxHash, TxKind, U256};
+use alloy::signers::Signature;
 use dashmap::DashMap;
+use reth_primitives::Recovered;
 use std::sync::Arc;
 use zk_ee::utils::Bytes32;
 use zk_os_forward_system::run::{BatchOutput, ExecutionResult};
-use zksync_os_types::{L2Envelope, L2Transaction};
+use zksync_os_types::{L1Transaction, L2Envelope, L2Transaction};
 
 #[derive(Clone, Debug)]
 pub struct TransactionApiData {
@@ -54,6 +59,82 @@ impl TransactionReceiptRepository {
 impl Default for TransactionReceiptRepository {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+pub fn l1_transaction_to_api_data(
+    block_output: &BatchOutput,
+    index: usize,
+    tx: L1Transaction,
+) -> TransactionApiData {
+    let tx_hash = TxHash::from(tx.hash().0);
+    let signer = Address::from(tx.common_data.sender.0);
+    let to = tx.execute.contract_address.map(|c| Address::from(c.0));
+    let tx_output = block_output
+        .tx_results
+        .iter()
+        .filter_map(|result| result.as_ref().ok())
+        .nth(index)
+        .expect("mismatch in number of transactions and results");
+
+    let tx_receipt = Receipt {
+        status: matches!(tx_output.execution_result, ExecutionResult::Success(_)).into(),
+        // todo
+        cumulative_gas_used: 7777,
+        // todo
+        logs: vec![],
+    };
+    let logs_bloom = tx_receipt.bloom_slow();
+    let receipt_with_bloom = ReceiptWithBloom::new(tx_receipt, logs_bloom);
+    // TODO: For now, pretend like L1 transactions are legacy for the purposes of API
+    //       Needs to be changed when we migrate L1 transactions to alloy
+    let receipt_envelope = ReceiptEnvelope::Legacy(receipt_with_bloom);
+    let rpc_receipt = alloy::rpc::types::TransactionReceipt {
+        inner: receipt_envelope,
+        transaction_hash: tx_hash,
+        transaction_index: Some(index as u64),
+        // block_hash: Some(BlockHash::from(block_output.header.hash())),
+        block_hash: Some(BlockHash::default()),
+        block_number: Some(block_output.header.number),
+        gas_used: tx_output.gas_used,
+        effective_gas_price: block_output.header.base_fee_per_gas as u128,
+        blob_gas_used: None,
+        blob_gas_price: None,
+        from: signer,
+        to,
+        contract_address: None,
+    };
+
+    let rpc_transaction = alloy::rpc::types::Transaction::from_transaction(
+        Recovered::new_unchecked(
+            L2Envelope::Legacy(Signed::new_unchecked(
+                TxLegacy {
+                    chain_id: Some(CHAIN_ID),
+                    nonce: tx.common_data.serial_id.0,
+                    gas_price: 0,
+                    gas_limit: tx.common_data.gas_limit.as_u64(),
+                    to: TxKind::Call(to.unwrap_or_default()),
+                    value: Default::default(),
+                    input: Default::default(),
+                },
+                Signature::new(U256::ZERO, U256::ZERO, false),
+                TxHash::from(tx.hash().0),
+            )),
+            signer,
+        ),
+        TransactionInfo {
+            hash: Some(tx_hash),
+            index: Some(index as u64),
+            // block_hash: Some(BlockHash::from(block_output.header.hash())),
+            block_hash: Some(BlockHash::default()),
+            block_number: Some(block_output.header.number),
+            base_fee: Some(block_output.header.base_fee_per_gas),
+        },
+    );
+
+    TransactionApiData {
+        transaction: rpc_transaction,
+        receipt: rpc_receipt,
     }
 }
 
