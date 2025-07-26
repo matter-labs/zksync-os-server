86d7808125dcb45844ee426fabf9dc7970295d12
Daniyar Itegulov <ditegulov@gmail.com>
2025-07-26 20:15:26 +1000

    chore: simplify prover test and add todo

--- integration-tests/tests/prover.rs
+++ integration-tests/tests/prover.rs
@@ -1,27 +1,18 @@
 #![cfg(feature = "prover-tests")]
 
-use alloy::network::TransactionBuilder;
-use alloy::primitives::{Address, U256};
-use alloy::providers::Provider;
-use alloy::rpc::types::TransactionRequest;
 use zksync_os_integration_tests::Tester;
-use zksync_os_integration_tests::assert_traits::ReceiptAssert;
 
 #[test_log::test(tokio::test)]
 async fn prover() -> anyhow::Result<()> {
     // Test that prover can successfully prove at least one batch
     let tester = Tester::builder().enable_prover().build().await?;
-    let tx = TransactionRequest::default()
-        .with_to(Address::random())
-        .with_value(U256::from(100));
-    tester
-        .l2_provider
-        .send_transaction(tx)
-        .await?
-        .expect_successful_receipt()
-        .await?;
 
+    // Test environment comes with some L1 transactions by default, so one batch should be provable
+    // without any new transactions inside the test.
     tester.prover_api.wait_for_batch_proven(1).await?;
+
+    // todo: consider expanding this test to prove multiple batches on top of the first batch
+    //       also to test L2 transactions are provable too
 
     Ok(())
 }
