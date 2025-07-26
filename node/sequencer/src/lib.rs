44f9e3d6c590e644838a6ef9ccf62f63e88cc5fa
Joonatan Saarhelo <joon.saar@gmail.com>
2025-07-26 13:14:18 +0100

    fix: remove alternative interfaces to account data (#109)
    Co-authored-by: Roman Brodetski <Roman.Brodetski@gmail.com>
    Co-authored-by: Daniyar Itegulov <ditegulov@gmail.com>

--- node/sequencer/src/lib.rs
+++ node/sequencer/src/lib.rs
@@ -302,7 +302,7 @@ pub async fn run(
 
     tracing::info!("Initializing mempools");
     let (l1_mempool, l2_mempool) = zksync_os_mempool::in_memory(
-        ZkClient::new(repositories.clone()),
+        ZkClient::new(repositories.clone(), state_handle.clone()),
         forced_deposit_transaction(),
         mempool_config.max_tx_input_bytes,
     );
