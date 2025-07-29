9bde493990a85b63951f14b6abe5c0f37f3c2fa3
Daniyar Itegulov <ditegulov@gmail.com>
2025-07-29 19:25:33 +1000

    test: make `l1_deposit` test assert receipt with L2->L1 logs

---
+++ integration-tests/src/network.rs
@@ -0,0 +1,216 @@
+use alloy::eips::eip2930::AccessList;
+use alloy::network::{
+    BuildResult, Network, NetworkWallet, TransactionBuilder, TransactionBuilderError,
+    UnbuiltTransactionError,
+};
+use alloy::primitives::{Address, Bytes, ChainId, TxKind, U256};
+use alloy::providers::fillers::{
+    ChainIdFiller, GasFiller, JoinFill, NonceFiller, RecommendedFillers,
+};
+use alloy::rpc::types::TransactionRequest;
+use serde::{Deserialize, Serialize};
+use zksync_os_types::{ZkReceiptEnvelope, ZkTxType};
+
+/// Dummy network that works on ZKsync OS-specific types.
+#[derive(Clone, Copy, Debug)]
+pub struct Zksync {
+    _private: (),
+}
+
+impl Network for Zksync {
+    type TxType = ZkTxType;
+
+    type TxEnvelope = alloy::consensus::TxEnvelope;
+
+    type UnsignedTx = alloy::consensus::TypedTransaction;
+
+    type ReceiptEnvelope = ZkReceiptEnvelope;
+
+    type Header = alloy::consensus::Header;
+
+    type TransactionRequest = ZkTransactionRequest;
+
+    type TransactionResponse = alloy::rpc::types::Transaction;
+
+    type ReceiptResponse =
+        alloy::rpc::types::TransactionReceipt<ZkReceiptEnvelope<alloy::rpc::types::Log>>;
+
+    type HeaderResponse = alloy::rpc::types::Header;
+
+    type BlockResponse = alloy::rpc::types::Block;
+}
+
+impl RecommendedFillers for Zksync {
+    type RecommendedFillers = JoinFill<GasFiller, JoinFill<NonceFiller, ChainIdFiller>>;
+
+    fn recommended_fillers() -> Self::RecommendedFillers {
+        Default::default()
+    }
+}
+
+#[derive(Clone, Debug, Default, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct ZkTransactionRequest(TransactionRequest);
+
+impl From<<Zksync as Network>::TxEnvelope> for ZkTransactionRequest {
+    fn from(value: <Zksync as Network>::TxEnvelope) -> Self {
+        Self(value.into())
+    }
+}
+
+impl From<<Zksync as Network>::UnsignedTx> for ZkTransactionRequest {
+    fn from(value: <Zksync as Network>::UnsignedTx) -> Self {
+        Self(value.into())
+    }
+}
+
+impl TransactionBuilder<Zksync> for ZkTransactionRequest {
+    fn chain_id(&self) -> Option<ChainId> {
+        TransactionBuilder::chain_id(&self.0)
+    }
+
+    fn set_chain_id(&mut self, chain_id: ChainId) {
+        TransactionBuilder::set_chain_id(&mut self.0, chain_id)
+    }
+
+    fn nonce(&self) -> Option<u64> {
+        TransactionBuilder::nonce(&self.0)
+    }
+
+    fn set_nonce(&mut self, nonce: u64) {
+        TransactionBuilder::set_nonce(&mut self.0, nonce)
+    }
+
+    fn take_nonce(&mut self) -> Option<u64> {
+        TransactionBuilder::take_nonce(&mut self.0)
+    }
+
+    fn input(&self) -> Option<&Bytes> {
+        TransactionBuilder::input(&self.0)
+    }
+
+    fn set_input<T: Into<Bytes>>(&mut self, input: T) {
+        TransactionBuilder::set_input(&mut self.0, input)
+    }
+
+    fn from(&self) -> Option<Address> {
+        TransactionBuilder::from(&self.0)
+    }
+
+    fn set_from(&mut self, from: Address) {
+        TransactionBuilder::set_from(&mut self.0, from)
+    }
+
+    fn kind(&self) -> Option<TxKind> {
+        TransactionBuilder::kind(&self.0)
+    }
+
+    fn clear_kind(&mut self) {
+        TransactionBuilder::clear_kind(&mut self.0)
+    }
+
+    fn set_kind(&mut self, kind: TxKind) {
+        TransactionBuilder::set_kind(&mut self.0, kind)
+    }
+
+    fn value(&self) -> Option<U256> {
+        TransactionBuilder::value(&self.0)
+    }
+
+    fn set_value(&mut self, value: U256) {
+        TransactionBuilder::set_value(&mut self.0, value)
+    }
+
+    fn gas_price(&self) -> Option<u128> {
+        TransactionBuilder::gas_price(&self.0)
+    }
+
+    fn set_gas_price(&mut self, gas_price: u128) {
+        TransactionBuilder::set_gas_price(&mut self.0, gas_price)
+    }
+
+    fn max_fee_per_gas(&self) -> Option<u128> {
+        TransactionBuilder::max_fee_per_gas(&self.0)
+    }
+
+    fn set_max_fee_per_gas(&mut self, max_fee_per_gas: u128) {
+        TransactionBuilder::set_max_fee_per_gas(&mut self.0, max_fee_per_gas)
+    }
+
+    fn max_priority_fee_per_gas(&self) -> Option<u128> {
+        TransactionBuilder::max_priority_fee_per_gas(&self.0)
+    }
+
+    fn set_max_priority_fee_per_gas(&mut self, max_priority_fee_per_gas: u128) {
+        TransactionBuilder::set_max_priority_fee_per_gas(&mut self.0, max_priority_fee_per_gas)
+    }
+
+    fn gas_limit(&self) -> Option<u64> {
+        TransactionBuilder::gas_limit(&self.0)
+    }
+
+    fn set_gas_limit(&mut self, gas_limit: u64) {
+        TransactionBuilder::set_gas_limit(&mut self.0, gas_limit)
+    }
+
+    fn access_list(&self) -> Option<&AccessList> {
+        TransactionBuilder::access_list(&self.0)
+    }
+
+    fn set_access_list(&mut self, access_list: AccessList) {
+        TransactionBuilder::set_access_list(&mut self.0, access_list)
+    }
+
+    fn complete_type(&self, ty: <Zksync as Network>::TxType) -> Result<(), Vec<&'static str>> {
+        match ty {
+            ZkTxType::L1 => {
+                unimplemented!()
+            }
+            ZkTxType::L2(ty) => TransactionBuilder::complete_type(&self.0, ty),
+        }
+    }
+
+    fn can_submit(&self) -> bool {
+        TransactionBuilder::can_submit(&self.0)
+    }
+
+    fn can_build(&self) -> bool {
+        TransactionBuilder::can_build(&self.0)
+    }
+
+    fn output_tx_type(&self) -> <Zksync as Network>::TxType {
+        ZkTxType::L2(TransactionBuilder::output_tx_type(&self.0))
+    }
+
+    fn output_tx_type_checked(&self) -> Option<<Zksync as Network>::TxType> {
+        Some(ZkTxType::L2(TransactionBuilder::output_tx_type_checked(
+            &self.0,
+        )?))
+    }
+
+    fn prep_for_submission(&mut self) {
+        TransactionBuilder::prep_for_submission(&mut self.0)
+    }
+
+    fn build_unsigned(self) -> BuildResult<<Zksync as Network>::UnsignedTx, Zksync> {
+        TransactionBuilder::build_unsigned(self.0).map_err(|e| UnbuiltTransactionError {
+            request: Self(e.request),
+            error: match e.error {
+                TransactionBuilderError::InvalidTransactionRequest(tx_type, keys) => {
+                    TransactionBuilderError::InvalidTransactionRequest(ZkTxType::L2(tx_type), keys)
+                }
+                TransactionBuilderError::UnsupportedSignatureType => {
+                    TransactionBuilderError::UnsupportedSignatureType
+                }
+                TransactionBuilderError::Signer(e) => TransactionBuilderError::Signer(e),
+                TransactionBuilderError::Custom(e) => TransactionBuilderError::Custom(e),
+            },
+        })
+    }
+
+    async fn build<W: NetworkWallet<Zksync>>(
+        self,
+        wallet: &W,
+    ) -> Result<<Zksync as Network>::TxEnvelope, TransactionBuilderError<Zksync>> {
+        Ok(wallet.sign_request(self).await?)
+    }
+}
---
+++ integration-tests/tests/l1.rs
@@ -0,0 +1,127 @@
+use alloy::eips::eip1559::Eip1559Estimation;
+use alloy::network::{TransactionBuilder, TxSigner};
+use alloy::primitives::{Address, B256, U256};
+use alloy::providers::utils::Eip1559Estimator;
+use alloy::providers::{PendingTransactionBuilder, Provider};
+use alloy::rpc::types::TransactionRequest;
+use std::str::FromStr;
+use zksync_os_contract_interface::Bridgehub;
+use zksync_os_contract_interface::IMailbox::NewPriorityRequest;
+use zksync_os_integration_tests::Tester;
+use zksync_os_integration_tests::assert_traits::ReceiptAssert;
+use zksync_os_types::{L2ToL1Log, ZkTxType};
+
+#[test_log::test(tokio::test)]
+async fn l1_deposit() -> anyhow::Result<()> {
+    // Test that we can deposit L2 funds from a rich L1 account
+    let tester = Tester::setup().await?;
+    let alice = tester.l1_wallet.default_signer().address();
+    let alice_l1_initial_balance = tester.l1_provider.get_balance(alice).await?;
+    let alice_l2_initial_balance = tester.l2_provider.get_balance(alice).await?;
+
+    // todo: copied over from alloy-zksync, use directly once it is EIP-712 agnostic
+    let bridgehub = Bridgehub::new(
+        Address::from_str("0x4b37536b9824c4a4cf3d15362135e346adb7cb9c").unwrap(),
+        tester.l1_provider.clone(),
+        270,
+    );
+    let amount = U256::from(100);
+    let gas_limit = U256::from(500_000);
+    let gas_per_pubdata = U256::from(800);
+    let max_priority_fee_per_gas = tester.l1_provider.get_max_priority_fee_per_gas().await?;
+    let base_l1_fees_data = tester
+        .l1_provider
+        .estimate_eip1559_fees_with(Eip1559Estimator::new(|base_fee_per_gas, _| {
+            Eip1559Estimation {
+                max_fee_per_gas: base_fee_per_gas * 3 / 2,
+                max_priority_fee_per_gas: 0,
+            }
+        }))
+        .await?;
+    let max_fee_per_gas = base_l1_fees_data.max_fee_per_gas + max_priority_fee_per_gas;
+    let tx_base_cost = bridgehub
+        .l2_transaction_base_cost(
+            U256::from(max_fee_per_gas + max_priority_fee_per_gas),
+            gas_limit,
+            gas_per_pubdata,
+        )
+        .await?;
+    let l1_deposit_request = bridgehub
+        .request_l2_transaction_direct(
+            amount + tx_base_cost,
+            alice,
+            amount,
+            vec![],
+            gas_limit,
+            gas_per_pubdata,
+            alice,
+        )
+        .value(amount + tx_base_cost)
+        .max_fee_per_gas(max_fee_per_gas)
+        .max_priority_fee_per_gas(max_priority_fee_per_gas)
+        .into_transaction_request();
+    let l1_deposit_receipt = tester
+        .l1_provider
+        .send_transaction(l1_deposit_request)
+        .await?
+        .expect_successful_receipt()
+        .await?;
+    let l1_to_l2_tx_log = l1_deposit_receipt
+        .logs()
+        .iter()
+        .filter_map(|log| log.log_decode::<NewPriorityRequest>().ok())
+        .next()
+        .expect("no L1->L2 logs produced by deposit tx");
+    let l2_tx_hash = l1_to_l2_tx_log.inner.txHash;
+
+    {
+        // fixme(#49): temporarily submit a random L2 transaction to make sequencer pick up new L1 tx
+        let tx = TransactionRequest::default()
+            .with_to(Address::random())
+            .with_value(U256::from(100));
+        tester
+            .l2_provider
+            .send_transaction(tx)
+            .await?
+            .expect_successful_receipt()
+            .await?;
+    }
+
+    let receipt = PendingTransactionBuilder::new(tester.l2_zk_provier.root().clone(), l2_tx_hash)
+        .expect_successful_receipt()
+        .await?;
+    assert_eq!(
+        receipt.inner.tx_type(),
+        ZkTxType::L1,
+        "expected L1->L2 deposit to produce an L1->L2 priority transaction"
+    );
+    let mut l2_to_l1_logs = receipt.inner.l2_to_l1_logs().to_vec();
+    assert_eq!(
+        l2_to_l1_logs.len(),
+        1,
+        "expected L1->L2 deposit transaction to only produce one L2->L1 log"
+    );
+    let l2_to_l1_log = l2_to_l1_logs.remove(0);
+    assert_eq!(
+        l2_to_l1_log,
+        L2ToL1Log {
+            // L1Messenger's address
+            sender: Address::from_str("0x0000000000000000000000000000000000008001").unwrap(),
+            // Canonical tx hash
+            key: l2_tx_hash,
+            // Successful
+            value: B256::from(U256::from(1)),
+        },
+        "expected L1->L2 deposit log to mark canonical tx hash as successful"
+    );
+
+    let fee = U256::from(l1_deposit_receipt.effective_gas_price)
+        * U256::from(l1_deposit_receipt.gas_used);
+
+    let alice_l1_final_balance = tester.l1_provider.get_balance(alice).await?;
+    let alice_l2_final_balance = tester.l2_provider.get_balance(alice).await?;
+    assert!(alice_l1_final_balance <= alice_l1_initial_balance - fee - amount);
+    assert!(alice_l2_final_balance >= alice_l2_initial_balance + amount);
+
+    Ok(())
+}
