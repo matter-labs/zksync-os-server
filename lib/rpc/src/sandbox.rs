2257273f1052c9c40196fedb018cdf1536528c15
Daniyar Itegulov <ditegulov@gmail.com>
2025-07-31 01:32:12 +1000

    feat: upgrade zksync-os to v0.0.8

--- lib/rpc/src/sandbox.rs
+++ lib/rpc/src/sandbox.rs
@@ -1,4 +1,5 @@
 use alloy::primitives::U256;
+use zk_ee::system::tracer::NopTracer;
 use zk_os_forward_system::run::errors::ForwardSubsystemError;
 use zk_os_forward_system::run::output::TxResult;
 use zk_os_forward_system::run::{BlockContext, simulate_tx};
@@ -19,5 +20,12 @@ pub fn execute(
 
     block_context.eip1559_basefee = U256::from(0);
 
-    simulate_tx(encoded_tx, block_context, state_view.clone(), state_view).map_err(Box::new)
+    simulate_tx(
+        encoded_tx,
+        block_context,
+        state_view.clone(),
+        state_view,
+        &mut NopTracer::default(),
+    )
+    .map_err(Box::new)
 }
