f1f8c3ee0baaeb8e390cc34b8d7470474ea858b5
Joonatan Saarhelo <joon.saar@gmail.com>
2025-08-12 15:27:15 +0200

    change RPC address for EN

--- run_en.sh
+++ run_en.sh
@@ -1,2 +1,3 @@
-sequencer_rocks_db_path=./db/en sequencer_is_external_node=true sequencer_prometheus_port=3313 \
+sequencer_is_external_node=true \
+sequencer_rocks_db_path=./db/en sequencer_prometheus_port=3313 rpc_address=0.0.0.0:3051 \
 cargo run --release
