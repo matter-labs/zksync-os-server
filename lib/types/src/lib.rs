690d118a2ec42f112d51715d11ccdb688f0f24fb
Daniyar Itegulov <ditegulov@gmail.com>
2025-08-19 15:25:40 +1000

    feat(api): populate RPC block size

--- lib/types/src/lib.rs
+++ lib/types/src/lib.rs
@@ -1,10 +1,15 @@
-mod log;
-mod receipt;
-pub mod rpc;
-mod transaction;
+mod block;
+pub use block::BlockExt;
 
+mod log;
 pub use log::{L2_TO_L1_TREE_SIZE, L2ToL1Log};
+
+mod receipt;
 pub use receipt::{ZkReceipt, ZkReceiptEnvelope};
+
+pub mod rpc;
+
+mod transaction;
 pub use transaction::{
     L1Envelope, L1EnvelopeError, L1TxSerialId, L2Envelope, L2Transaction, TxL1Priority, ZkEnvelope,
     ZkTransaction, ZkTxType, ZksyncOsEncode,
