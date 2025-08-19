ce24acf026ace7a49f0271ed03e8e3da6816a863
Daniyar Itegulov <ditegulov@gmail.com>
2025-08-19 16:58:26 +1000

    feat(api): populate RPC block size (#217)

--- lib/types/src/rpc.rs
+++ lib/types/src/rpc.rs
@@ -1,4 +1,8 @@
-use alloy::primitives::B256;
+use crate::{BlockExt, ZkEnvelope, ZkReceiptEnvelope};
+use alloy::consensus::Sealed;
+use alloy::network::primitives::BlockTransactions;
+use alloy::primitives::{B256, TxHash, U256};
+use alloy::rpc::types::Log;
 use serde::{Deserialize, Serialize};
 
 /// A struct with the proof for the L2->L1 log in a specific block.
@@ -12,4 +16,25 @@ pub struct L2ToL1LogProof {
     pub id: u32,
     /// The root of the tree.
     pub root: B256,
+}
+
+pub type ZkTransaction = alloy::rpc::types::Transaction<ZkEnvelope>;
+pub type ZkTransactionReceipt = alloy::rpc::types::TransactionReceipt<ZkReceiptEnvelope<Log>>;
+pub type ZkBlock = alloy::rpc::types::Block<ZkTransaction>;
+pub type ZkHeader = alloy::rpc::types::Header;
+
+pub trait RpcBlockConvert {
+    fn into_rpc(self) -> ZkBlock;
+}
+
+impl RpcBlockConvert for Sealed<alloy::consensus::Block<TxHash>> {
+    fn into_rpc(self) -> ZkBlock {
+        let hash = self.hash();
+        let block = self.unseal();
+        let rlp_length = block.rlp_length();
+        ZkBlock::new(
+            ZkHeader::from_consensus(block.header.seal(hash), None, Some(U256::from(rlp_length))),
+            BlockTransactions::Hashes(block.body.transactions),
+        )
+    }
 }
