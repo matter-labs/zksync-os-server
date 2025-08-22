dc3ac6231518c2e964750555bc92a3c45200f712
romanbrodetski <rb@matterlabs.dev>
2025-08-22 09:49:55 +0300

    feat: more metrics

--- node/sequencer/src/batcher/seal_criteria.rs
+++ node/sequencer/src/batcher/seal_criteria.rs
@@ -1,6 +1,7 @@
 use zk_ee::common_structs::MAX_NUMBER_OF_LOGS;
 use zk_ee::system::MAX_NATIVE_COMPUTATIONAL;
 use zk_os_forward_system::run::BlockOutput;
+use zksync_os_l1_sender::batcher_metrics::BATCHER_METRICS;
 
 #[derive(Default, Clone)]
 pub(crate) struct BatchInfoAccumulator {
@@ -39,25 +40,38 @@ impl BatchInfoAccumulator {
     /// e.g. due to the block count limit, tx count limit, or pubdata size limit.
     pub fn is_batch_limit_reached(&self, blocks_len: usize) -> bool {
         if blocks_len > self.blocks_per_batch_limit {
+            BATCHER_METRICS.seal_reason[&"blocks_per_batch"].inc();
             tracing::debug!("Batcher: reached blocks per batch limit");
             return true;
         }
 
         if self.native_cycles > MAX_NATIVE_COMPUTATIONAL {
+            BATCHER_METRICS.seal_reason[&"native_cycles"].inc();
             tracing::debug!("Batcher: reached native cycles limit for the batch");
             return true;
         }
 
         if self.pubdata_bytes > self.batch_pubdata_limit_bytes {
+            BATCHER_METRICS.seal_reason[&"pubdata"].inc();
             tracing::debug!("Batcher: reached pubdata bytes limit for the batch");
             return true;
         }
 
         if self.l2_to_l1_logs_count > MAX_NUMBER_OF_LOGS {
+            BATCHER_METRICS.seal_reason[&"l2_l1_logs"].inc();
             tracing::debug!("Batcher: reached max number of L2 to L1 logs");
             return true;
         }
 
         false
+    }
+
+    pub fn report_accumulated_resources_to_metrics(&self) {
+        BATCHER_METRICS
+            .computational_native_used_per_batch
+            .observe(self.native_cycles);
+        BATCHER_METRICS
+            .pubdata_per_batch
+            .observe(self.pubdata_bytes);
     }
 }
