31b530c898ae605b5c88f6b5403b49ce2f879d75
Roman Brodetski <Roman.Brodetski@gmail.com>
2025-08-25 15:43:34 +0300

    chore: more logging on errors (#258)

--- node/sequencer/src/execution/block_executor.rs
+++ node/sequencer/src/execution/block_executor.rs
@@ -33,7 +33,7 @@ pub async fn execute_block(
         .map_err(|e| BlockDump {
             ctx,
             txs: Vec::new(),
-            error: e.to_string(),
+            error: e.context("state.state_view_at_block()").to_string(),
         })?;
     let mut runner = VmWrapper::new(ctx, state_view);
 
@@ -113,7 +113,7 @@ pub async fn execute_block(
                                             BlockDump {
                                                 ctx,
                                                 txs: all_processed_txs.clone(),
-                                                error: format!("invalid {} tx: {e:?}", tx.tx_type()),
+                                                error: format!("invalid {} tx: {e:?} ({})", tx.tx_type(), tx.hash()),
                                             }
                                         )
                                     }
@@ -142,7 +142,7 @@ pub async fn execute_block(
                                                 BlockDump {
                                                     ctx,
                                                     txs: all_processed_txs.clone(),
-                                                    error: format!("invalid l2 tx: {e:?}"),
+                                                    error: format!("invalid l2 tx: {e:?} ({})", tx.hash()),
                                                 }
                                             )
                                     }
@@ -159,6 +159,11 @@ pub async fn execute_block(
                                 ctx.block_number,
                                 cumulative_gas_used + tx.inner.gas_limit(),
                                 ctx.gas_limit
+                            );
+                            let partial_seal_block_result = runner.seal_block().await;
+                            tracing::error!(
+                                ?partial_seal_block_result,
+                                error
                             );
                             return Err(
                                 BlockDump {
@@ -236,6 +241,8 @@ pub async fn execute_block(
         "Block sealed in block executor"
     );
 
+    tracing::debug!(?output, "Block output");
+
     let block_hash_output = hash_block_output(&output);
 
     // Check if the block output matches the expected hash.
@@ -246,6 +253,7 @@ pub async fn execute_block(
             "Block #{} output hash mismatch: expected {expected_hash}, got {block_hash_output}",
             ctx.block_number,
         );
+        tracing::error!(?output, error);
         return Err(BlockDump {
             ctx,
             txs: all_processed_txs.clone(),
