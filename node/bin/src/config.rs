0ccfa9da3754060acbde2397a3b1ce6fcf8d7e32
perekopskiy <mikeson.dp@gmail.com>
2025-08-28 14:24:24 +0300

    update state

--- node/bin/src/config.rs
+++ node/bin/src/config.rs
@@ -183,7 +183,7 @@ pub struct GenesisConfig {
     /// L1 address of `Bridgehub` contract. This address and chain ID is an entrypoint into L1 discoverability so most
     /// other contracts should be discoverable through it.
     // TODO: Pre-configured value, to be removed
-    #[config(with = Serde![str], default_t = "0xf7d7d5ca0490c2c9d263209abefdc52febd2bc80".parse().unwrap())]
+    #[config(with = Serde![str], default_t = "0x133303087fc98a0371c422a1e89abd66d8763e73".parse().unwrap())]
     pub bridgehub_address: Address,
 
     /// Chain ID of the chain node operates on.
