3cd890c30634a372c44f3ca5e01788d2c258b3fe
dimazhornyk <dima.zhornyk@gmail.com>
2025-09-10 18:33:33 +0200

    merge two methods to run status server

--- lib/status/src/lib.rs
+++ lib/status/src/lib.rs
@@ -10,10 +10,10 @@ struct AppState {
     stop_receiver: watch::Receiver<bool>,
 }
 
-pub async fn prepare_status_server(
+pub async fn run_status_server(
     bind_address: String,
     stop_receiver: watch::Receiver<bool>,
-) -> anyhow::Result<(TcpListener, Router)> {
+) -> anyhow::Result<()> {
     let app = Router::new()
         .route("/status/health", get(health))
         .with_state(AppState { stop_receiver });
@@ -21,10 +21,6 @@ pub async fn prepare_status_server(
     let addr: SocketAddr = bind_address.parse()?;
     let listener = TcpListener::bind(addr).await?;
 
-    Ok((listener, app))
-}
-
-pub async fn run_status_server(listener: TcpListener, app: Router) -> anyhow::Result<()> {
     let addr = listener.local_addr()?;
     tracing::info!("running a status server" = %addr);
 
