3a41cec630ce94085b857020bcc077dc304c7dba
Joonatan Saarhelo <joon.saar@gmail.com>
2025-09-11 17:45:27 +0200

    update EN script

--- run_en.sh
+++ run_en.sh
@@ -1,4 +1,4 @@
-block_replay_download_address=localhost:3050 \
-general_public_port=3053 general_private_port=3054 \
-sequencer_rocks_db_path=./db/en \
+sequencer_block_replay_download_address=localhost:3050 \
+general_public_address=localhost:3053 general_private_address=localhost:3054 \
+general_rocks_db_path=./db/en \
 cargo run --release
