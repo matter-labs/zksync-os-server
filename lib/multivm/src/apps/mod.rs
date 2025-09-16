bcc5aa5cc038b2f73dccccb7d294a590b464d56d
perekopskiy <mikeson.dp@gmail.com>
2025-09-16 10:53:49 +0300

    check version

--- lib/multivm/src/apps/mod.rs
+++ lib/multivm/src/apps/mod.rs
@@ -1,12 +1,14 @@
 pub mod v1 {
-    pub const SERVER_APP: &[u8] =
-        include_bytes!(concat!(env!("ZKSYNC_OS_SOURCE_PATH"), "/server_app.bin"));
+    pub const SERVER_APP: &[u8] = include_bytes!(concat!(
+        env!("ZKSYNC_OS_0_0_22_SOURCE_PATH"),
+        "/server_app.bin"
+    ));
     pub const SERVER_APP_LOGGING_ENABLED: &[u8] = include_bytes!(concat!(
-        env!("ZKSYNC_OS_SOURCE_PATH"),
+        env!("ZKSYNC_OS_0_0_22_SOURCE_PATH"),
         "/server_app_logging_enabled.bin"
     ));
     pub const MULTIBLOCK_BATCH: &[u8] = include_bytes!(concat!(
-        env!("ZKSYNC_OS_SOURCE_PATH"),
+        env!("ZKSYNC_OS_0_0_22_SOURCE_PATH"),
         "/multiblock_batch.bin"
     ));
 }
