74adc8e6300657a6d34584903749f57d23925674
perekopskiy <mikeson.dp@gmail.com>
2025-09-24 15:37:40 +0300

    update zksync-os

--- lib/multivm/src/apps/mod.rs
+++ lib/multivm/src/apps/mod.rs
@@ -70,7 +70,7 @@ pub mod v2 {
     use std::sync::OnceLock;
 
     pub const SERVER_APP: &[u8] = include_bytes!(concat!(
-        env!("ZKSYNC_OS_0_0_24_SOURCE_PATH"),
+        env!("ZKSYNC_OS_0_0_25_SOURCE_PATH"),
         "/server_app.bin"
     ));
 
@@ -89,7 +89,7 @@ pub mod v2 {
     }
 
     pub const SERVER_APP_LOGGING_ENABLED: &[u8] = include_bytes!(concat!(
-        env!("ZKSYNC_OS_0_0_24_SOURCE_PATH"),
+        env!("ZKSYNC_OS_0_0_25_SOURCE_PATH"),
         "/server_app_logging_enabled.bin"
     ));
 
@@ -108,7 +108,7 @@ pub mod v2 {
     }
 
     pub const MULTIBLOCK_BATCH: &[u8] = include_bytes!(concat!(
-        env!("ZKSYNC_OS_0_0_24_SOURCE_PATH"),
+        env!("ZKSYNC_OS_0_0_25_SOURCE_PATH"),
         "/multiblock_batch.bin"
     ));
 
