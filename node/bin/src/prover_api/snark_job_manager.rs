d9e471a90d3633c6ad39cb55ff0db97322e1df43
perekopskiy <mikeson.dp@gmail.com>
2025-09-25 16:02:54 +0300

    fix pick_real_job

--- node/bin/src/prover_api/snark_job_manager.rs
+++ node/bin/src/prover_api/snark_job_manager.rs
@@ -100,7 +100,7 @@ impl SnarkJobManager {
         let first_version = batches_with_real_proofs[0]
             .1
             .proving_execution_version()
-            .unwrap();
+            .unwrap_or(1); // if version is missing, assume it's 1
         let batches_with_real_proofs: Vec<_> = batches_with_real_proofs
             .into_iter()
             .take_while(|(_, p)| p.proving_execution_version() == Some(first_version))
