ba8e72f1845ba2f734c94ad397e426737fd77c36
Roman Brodetski <Roman.Brodetski@gmail.com>
2025-09-29 15:47:00 +0100

    revert: feat: set gas per pubdata to `1` (#431)
    Reverts matter-labs/zksync-os-server#406

--- lib/sequencer/src/execution/block_context_provider.rs
+++ lib/sequencer/src/execution/block_context_provider.rs
@@ -90,8 +90,7 @@ impl<Mempool: L2TransactionPool> BlockContextProvider<Mempool> {
                 let block_context = BlockContext {
                     eip1559_basefee: U256::from(1000),
                     native_price: U256::from(1),
-                    // todo: make dynamic once zksync-os sets max gas per pubdata >1 for L2 txs
-                    gas_per_pubdata: U256::from(1),
+                    gas_per_pubdata: Default::default(),
                     block_number: produce_command.block_number,
                     timestamp,
                     chain_id: self.chain_id,
