193e048017e250bab0e4025ce6898957d57744df
Daniyar Itegulov <ditegulov@gmail.com>
2025-09-29 22:00:24 +1000

    feat: use interface tracing API

--- lib/multivm/build.rs
+++ lib/multivm/build.rs
@@ -8,11 +8,11 @@ fn main() {
     for package in &metadata.packages {
         for version in &versions {
             if package.name.as_str() == "forward_system"
-                && package.source.as_ref().is_some_and(|s| {
-                    s.to_string().contains(&format!(
-                        "https://github.com/matter-labs/zksync-os?tag=v{version}"
-                    ))
-                })
+            // && package.source.as_ref().is_some_and(|s| {
+            //     s.to_string().contains(&format!(
+            //         "https://github.com/matter-labs/zksync-os?tag=v{version}"
+            //     ))
+            // })
             {
                 let forward_system_source = package.manifest_path.parent().unwrap();
                 let zksync_os_source = forward_system_source.parent().unwrap().join("zksync_os");
