63f9f69fcb6486f9e57dc983c7efcf14c0623a69
Daniyar Itegulov <ditegulov@gmail.com>
2025-10-07 20:41:49 +1100

    feat: make mempool configurable (#464)
    Fixes #432 
    
    Moves existing mempool configuration to `TxValidatorConfig` (because
    that's what it actually is) and introduces new configuration based off
    of reth's mempool `PoolConfig` (only max pending txs and max pending txs
    size for now).

--- lib/mempool/src/lib.rs
+++ lib/mempool/src/lib.rs
@@ -10,31 +10,36 @@ pub use traits::L2TransactionPool;
 mod transaction;
 pub use transaction::L2PooledTransaction;
 
+mod config;
+pub use config::TxValidatorConfig;
+
 // Re-export some of the reth mempool's types.
 pub use reth_transaction_pool::error::PoolError;
 pub use reth_transaction_pool::{
-    CanonicalStateUpdate, NewSubpoolTransactionStream, NewTransactionEvent, PoolUpdateKind,
-    TransactionPool as RethTransactionPool, TransactionPoolExt as RethTransactionPoolExt,
+    CanonicalStateUpdate, NewSubpoolTransactionStream, NewTransactionEvent, PoolConfig,
+    PoolUpdateKind, SubPoolLimit, TransactionPool as RethTransactionPool,
+    TransactionPoolExt as RethTransactionPoolExt,
 };
 
 use reth_chainspec::{ChainSpecProvider, EthereumHardforks};
 use reth_storage_api::StateProviderFactory;
+use reth_transaction_pool::CoinbaseTipOrdering;
 use reth_transaction_pool::blobstore::NoopBlobStore;
 use reth_transaction_pool::validate::EthTransactionValidatorBuilder;
-use reth_transaction_pool::{CoinbaseTipOrdering, PoolConfig};
 
 pub fn in_memory<Client: ChainSpecProvider<ChainSpec: EthereumHardforks> + StateProviderFactory>(
     client: Client,
-    max_input_bytes: usize,
+    pool_config: PoolConfig,
+    validator_config: TxValidatorConfig,
 ) -> RethPool<Client> {
     let blob_store = NoopBlobStore::default();
     RethPool::new(
         EthTransactionValidatorBuilder::new(client)
             .no_prague()
-            .with_max_tx_input_bytes(max_input_bytes)
+            .with_max_tx_input_bytes(validator_config.max_input_bytes)
             .build(blob_store),
         CoinbaseTipOrdering::default(),
         blob_store,
-        PoolConfig::default(),
+        pool_config,
     )
 }
