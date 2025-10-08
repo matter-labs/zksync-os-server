074220991386e6a26467fdd703d2157faf1c77fc
Roman Brodetski <Roman.Brodetski@gmail.com>
2025-10-08 08:45:59 +0100

    feat: pipeline framework (5/X) - migrate l1 committer (#472)
    Migrating l1 committer. - very limited set of changes

---
+++ lib/l1_sender/src/pipeline_component.rs
@@ -0,0 +1,46 @@
+use crate::batcher_model::{BatchEnvelope, FriProof};
+use crate::commands::L1SenderCommand;
+use crate::config::L1SenderConfig;
+use crate::run_l1_sender;
+use alloy::network::EthereumWallet;
+use alloy::primitives::Address;
+use alloy::providers::{Provider, WalletProvider};
+use async_trait::async_trait;
+use tokio::sync::mpsc;
+use zksync_os_pipeline::{PeekableReceiver, PipelineComponent};
+
+/// Generic L1 Sender pipeline component
+/// Can be used for commit, prove, or execute operations
+pub struct L1Sender<P, C> {
+    pub provider: P,
+    pub config: L1SenderConfig<C>,
+    pub to_address: Address,
+}
+
+#[async_trait]
+impl<P, C> PipelineComponent for L1Sender<P, C>
+where
+    P: Provider + WalletProvider<Wallet = EthereumWallet> + Clone + Send + 'static,
+    C: L1SenderCommand + Send + Sync + 'static,
+{
+    type Input = C;
+    type Output = BatchEnvelope<FriProof>;
+
+    const NAME: &'static str = C::NAME;
+    const OUTPUT_BUFFER_SIZE: usize = 5;
+
+    async fn run(
+        self,
+        input: PeekableReceiver<Self::Input>,
+        output: mpsc::Sender<Self::Output>,
+    ) -> anyhow::Result<()> {
+        run_l1_sender(
+            input.into_inner(),
+            output,
+            self.to_address,
+            self.provider,
+            self.config,
+        )
+        .await
+    }
+}
