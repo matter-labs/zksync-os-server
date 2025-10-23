c42aafd2e72dffd982966a13d49e533194894a0b
Vlad Bochok <vladbochok1@gmail.com>
2025-10-23 17:11:22 +0200

    Send output after processing the block

--- lib/revm_consistency_checker/src/node.rs
+++ lib/revm_consistency_checker/src/node.rs
@@ -59,8 +59,59 @@ where
                 anyhow::bail!("inbound channel closed");
             };
 
+            latency_tracker.enter_state(GenericComponentState::Processing);
+            self.state.set_latest_block(
+                replay_record.block_context.block_number - 1,
+                replay_record.block_context.block_hashes,
+            );
+
+            {
+                // For each block, we create an in-memory cache database to accumulate transaction state changes separately
+                let mut cache_db = CacheDB::new(self.state.clone());
+                let mut evm = Context::default()
+                    .with_db(&mut cache_db)
+                    .modify_cfg_chained(|cfg| {
+                        cfg.chain_id = replay_record.block_context.chain_id;
+                    })
+                    .modify_block_chained(|block| {
+                        block.number = U256::from(replay_record.block_context.block_number);
+                        block.timestamp = U256::from(replay_record.block_context.timestamp);
+                        block.beneficiary = replay_record.block_context.coinbase;
+                        block.basefee = replay_record.block_context.eip1559_basefee.saturating_to();
+                        block.gas_limit = replay_record.block_context.gas_limit;
+                        // `replay_record.block_context` holds an incorrect `prevrandao` value.
+                        // We use the actual value that ZKsync OS uses instead.
+                        block.prevrandao = Some(U256::ONE.into());
+                    })
+                    .build_zk();
+
+                let revm_txs = replay_record
+                    .transactions
+                    .iter()
+                    .zip(&block_output.tx_results)
+                    .filter_map(|(transaction, tx_output_raw)| {
+                        let tx_output = match tx_output_raw {
+                            Ok(tx_output) => tx_output,
+                            _ => return None, // Skip invalid transaction as they are not included in the batch
+                        };
+
+                        Some(zk_tx_into_revm_tx(
+                            transaction,
+                            tx_output.gas_used,
+                            tx_output.is_success(),
+                        ))
+                    });
+
+                evm.transact_many_commit(revm_txs)?;
+                let compare_report = CompareReport::build(
+                    evm.0.db_mut(),
+                    &block_output.storage_writes,
+                    &block_output.account_diffs,
+                )?;
+                compare_report.log_tracing(20);
+            }
+
             latency_tracker.enter_state(GenericComponentState::WaitingSend);
-            // Immediately send the output to avoid blocking the next pipeline steps
             if output
                 .send((block_output.clone(), replay_record.clone()))
                 .await
@@ -68,55 +119,6 @@ where
             {
                 anyhow::bail!("Outbound channel closed");
             }
-            latency_tracker.enter_state(GenericComponentState::Processing);
-
-            self.state.set_latest_block(
-                replay_record.block_context.block_number - 1,
-                replay_record.block_context.block_hashes,
-            );
-            // For each block, we create an in-memory cache database to accumulate transaction state changes separately
-            let mut cache_db = CacheDB::new(self.state.clone());
-            let mut evm = Context::default()
-                .with_db(&mut cache_db)
-                .modify_cfg_chained(|cfg| {
-                    cfg.chain_id = replay_record.block_context.chain_id;
-                })
-                .modify_block_chained(|block| {
-                    block.number = U256::from(replay_record.block_context.block_number);
-                    block.timestamp = U256::from(replay_record.block_context.timestamp);
-                    block.beneficiary = replay_record.block_context.coinbase;
-                    block.basefee = replay_record.block_context.eip1559_basefee.saturating_to();
-                    block.gas_limit = replay_record.block_context.gas_limit;
-                    // `replay_record.block_context` holds an incorrect `prevrandao` value.
-                    // We use the actual value that ZKsync OS uses instead.
-                    block.prevrandao = Some(U256::ONE.into());
-                })
-                .build_zk();
-
-            let revm_txs = replay_record
-                .transactions
-                .iter()
-                .zip(block_output.tx_results)
-                .filter_map(|(transaction, tx_output_raw)| {
-                    let tx_output = match tx_output_raw {
-                        Ok(tx_output) => tx_output,
-                        _ => return None, // Skip invalid transaction as they are not included in the batch
-                    };
-
-                    Some(zk_tx_into_revm_tx(
-                        transaction,
-                        tx_output.gas_used,
-                        tx_output.is_success(),
-                    ))
-                });
-
-            evm.transact_many_commit(revm_txs)?;
-            let compare_report = CompareReport::build(
-                evm.0.db_mut(),
-                &block_output.storage_writes,
-                &block_output.account_diffs,
-            )?;
-            compare_report.log_tracing(20);
         }
     }
 }
