aac3bc751892f379ffdb2e2ffcc85b6550cc5505
Artur Puzio <contact@puzio.waw.pl>
2025-10-28 14:58:13 +0100

    Move BlockCache to file

--- lib/batch_verification/src/client/mod.rs
+++ lib/batch_verification/src/client/mod.rs
@@ -7,7 +7,6 @@ use alloy::signers::local::PrivateKeySigner;
 use async_trait::async_trait;
 use futures::{SinkExt, StreamExt};
 use secrecy::{ExposeSecret, SecretString};
-use std::collections::HashMap;
 use std::str::FromStr;
 use std::time::Duration;
 use structdiff::StructDiff;
@@ -28,62 +27,8 @@ use zksync_os_socket::connect;
 use zksync_os_storage_api::ReadFinality;
 use zksync_os_storage_api::ReplayRecord;
 
-/// Cache of blocks that are to be used for batch verification
-/// Accepts blocks only in ascending order. Old blocks are evicted when not
-/// needed through a call to remove_lower_then.
-///
-/// This may be optimized by using a ring buffer for data storage instead.
-///
-/// TODO metric for blocks in signing cache
-struct BlockCache<Finality> {
-    data: HashMap<u64, (BlockOutput, ReplayRecord, BlockMerkleTreeData)>,
-    range: Option<(u64, u64)>,
-    finality: Finality,
-}
-
-impl<Finality: ReadFinality> BlockCache<Finality> {
-    fn new(finality: Finality) -> Self {
-        Self {
-            data: HashMap::new(),
-            range: None,
-            finality,
-        }
-    }
-
-    /// Insert a block into the cache. Expected blocks to be added in order.
-    fn insert(
-        &mut self,
-        block_number: u64,
-        block: (BlockOutput, ReplayRecord, BlockMerkleTreeData),
-    ) -> anyhow::Result<()> {
-        self.data.insert(block_number, block);
-        if let Some((low, high)) = self.range {
-            if block_number != high + 1 {
-                anyhow::bail!("Out of order block received. This should never happen");
-            }
-            self.range = Some((low, block_number));
-        } else {
-            self.range = Some((block_number, block_number));
-        }
-
-        // evict block for committed batches
-        self.remove_lower_then(self.finality.get_finality_status().last_committed_block + 1);
-        Ok(())
-    }
-
-    fn get(&self, block_number: u64) -> Option<&(BlockOutput, ReplayRecord, BlockMerkleTreeData)> {
-        self.data.get(&block_number)
-    }
-
-    /// Removes all blocks lower than the given block number
-    fn remove_lower_then(&mut self, block_number: u64) {
-        if let Some((low, _)) = self.range {
-            for num in low..block_number {
-                self.data.remove(&num);
-            }
-        }
-    }
-}
+mod block_cache;
+use block_cache::BlockCache;
 
 /// Client that connects to the main sequencer for batch verification
 pub struct BatchVerificationClient<Finality> {
