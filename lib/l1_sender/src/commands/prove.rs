9a0e8512295fbf5a8f9a598f86a4d3b37a9d3f7a
EmilLuta <virgil.luta@gmail.com>
2025-11-03 08:09:44 +0100

    Update error message for unsupport exec_version

--- lib/l1_sender/src/commands/prove.rs
+++ lib/l1_sender/src/commands/prove.rs
@@ -138,7 +138,9 @@ impl ProofCommand {
             // v2 and up are available under their respective execution version.
             Some(2) => 2,
             Some(3) => 3,
-            Some(execution_version) => panic!("unsupported verification key: {execution_version}"),
+            Some(execution_version) => panic!(
+                "unsupported execution version: {execution_version}; there's no verifier defined for it"
+            ),
         };
 
         // todo: remove tostring
