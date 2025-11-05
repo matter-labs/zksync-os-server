61531bb746608df45ab9cf1111055dc6b4880a1f
Artur Puzio <contact@puzio.waw.pl>
2025-11-05 13:16:24 +0100

    document batch verification

--- run_server_batch_verification.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Run the main sequencer/server with batch verification enabled
-# This requires at least one external node to connect and sign batches
-
-batch_verification_server_enabled=true \
-rust_log=info,zksync_os_batch_verification=debug \
-cargo run #--release
