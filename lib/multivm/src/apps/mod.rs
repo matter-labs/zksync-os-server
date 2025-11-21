c1bd0dd613141e4043ea5b0fd14cdeb8cd6afe9e
perekopskiy <mikeson.dp@gmail.com>
2025-11-21 15:30:44 +0200

    update zksync-os with p256 fix

--- lib/multivm/src/apps/mod.rs
+++ lib/multivm/src/apps/mod.rs
@@ -3,7 +3,7 @@ pub mod v4 {
     use std::sync::OnceLock;
 
     pub const SINGLEBLOCK_BATCH_APP: &[u8] = include_bytes!(concat!(
-        env!("ZKSYNC_OS_0_1_0_SOURCE_PATH"),
+        env!("ZKSYNC_OS_0_1_0-rc1_SOURCE_PATH"),
         "/singleblock_batch.bin"
     ));
 
@@ -27,7 +27,7 @@ pub mod v4 {
     }
 
     pub const SINGLEBLOCK_BATCH_LOGGING_ENABLED: &[u8] = include_bytes!(concat!(
-        env!("ZKSYNC_OS_0_1_0_SOURCE_PATH"),
+        env!("ZKSYNC_OS_0_1_0-rc1_SOURCE_PATH"),
         "/singleblock_batch_logging_enabled.bin"
     ));
 
@@ -51,7 +51,7 @@ pub mod v4 {
     }
 
     pub const MULTIBLOCK_BATCH: &[u8] = include_bytes!(concat!(
-        env!("ZKSYNC_OS_0_1_0_SOURCE_PATH"),
+        env!("ZKSYNC_OS_0_1_0-rc1_SOURCE_PATH"),
         "/multiblock_batch.bin"
     ));
 
