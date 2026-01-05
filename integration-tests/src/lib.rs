d66b40f5ac618ce63317f2491327023caa395113
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2026-01-05 17:23:16 +0200

    fix: integration tests

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -232,7 +232,7 @@ impl Tester {
             general_config,
             genesis_config: GenesisConfig {
                 genesis_input_path: Some(
-                    concat!(env!("WORKSPACE_DIR"), "/genesis/genesis.json").into(),
+                    concat!(env!("WORKSPACE_DIR"), "/local-chains/v30/genesis.json").into(),
                 ),
                 ..Default::default()
             },
