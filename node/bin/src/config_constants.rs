9b431fef5cd5964528b84cc72f37d4a68ee967b9
Danil <deniallugo@gmail.com>
2026-01-06 15:19:10 +0000

    Fix constants
    Signed-off-by: Danil <deniallugo@gmail.com>

--- node/bin/src/config_constants.rs
+++ node/bin/src/config_constants.rs
@@ -8,11 +8,11 @@ pub const DEFAULT_ROCKS_DB_PATH: &str = "./db/node1";
 
 /// L1 address of `Bridgehub` contract. This address and chain ID is an entrypoint into L1 discoverability so most
 /// other contracts should be discoverable through it.
-pub const BRIDGEHUB_ADDRESS: &str = "0x0767b13ae281dd3a4cf963d9f1e26719e4dea04e";
+pub const BRIDGEHUB_ADDRESS: &str = "0xff945289dd69c4d6ad70310646e579f72f53f509";
 
 /// L1 address of the `BytecodeSupplier` contract. This address right now cannot be discovered through `Bridgehub`,
 /// so it has to be provided explicitly.
-pub const BYTECODE_SUPPLIER_ADDRESS: &str = "0xc40e7810e36888598341af0436bd17490eff3f14";
+pub const BYTECODE_SUPPLIER_ADDRESS: &str = "0x3c5d3ea7c81e5f2d53039b5edfc429d6fa7e06fb";
 
 /// Chain ID of the chain node operates on.
 pub const CHAIN_ID: u64 = 6565;
@@ -20,14 +20,14 @@ pub const CHAIN_ID: u64 = 6565;
 /// Private key to commit batches to L1
 /// Must be consistent with the operator key set on the contract (permissioned!)
 #[rustfmt::skip]
-pub const OPERATOR_COMMIT_PK: &str = "0x25c9b0b2560a79e646e8a3fb2eb997b5128d033943100b4b8a7dead752b34b79";
+pub const OPERATOR_COMMIT_PK: &str = "0x337b8bc9d6e7e025d30ff6fe04a3f9a5442d7ded5ad8457d6b9587a93a9e6e60";
 
 /// Private key to use to submit proofs to L1
 /// Can be arbitrary funded address - proof submission is permissionless.
 #[rustfmt::skip]
-pub const OPERATOR_PROVE_PK: &str = "0x6380e89a46923b6d62877f374981f47635ca36ccc9706580db99d5f29440f9b4";
+pub const OPERATOR_PROVE_PK: &str = "0x4b53d6d3929559689c931c7837b409ec534bc0f1e557f15100a904ca921b8c3a";
 
 /// Private key to use to execute batches on L1
 /// Can be arbitrary funded address - execute submission is permissionless.
 #[rustfmt::skip]
-pub const OPERATOR_EXECUTE_PK: &str = "0x854107184439c97d4a1f079aef4f34ecc764179b1159466dec9d29cffefca9cf";
+pub const OPERATOR_EXECUTE_PK: &str = "0x3a5799dac64d9aab4b5e39f715b011df730165f6ca21aed588ae30fd7bd5f5e1";
