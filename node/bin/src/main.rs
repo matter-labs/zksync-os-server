426354ca932e1dc9f95b3e6ddffafeb6e8112fa2
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2026-01-08 12:36:56 +0200

    fix: formatting

--- node/bin/src/main.rs
+++ node/bin/src/main.rs
@@ -33,8 +33,8 @@ enum CliCommand {
 #[derive(Debug, Parser)]
 #[command(author = "Matter Labs", version, about = "ZKsync OS node", long_about = None)]
 struct Cli {
-    /// Path to a JSON config file. If not specified, default config will attempted to be loaded to fill in the config 
-    /// values for local setup. If default config is missing, no configs will be loaded, and they must be explicitly set 
+    /// Path to a JSON config file. If not specified, default config will attempted to be loaded to fill in the config
+    /// values for local setup. If default config is missing, no configs will be loaded, and they must be explicitly set
     /// via other configuration means (e.g. environment variables). Env variables override config settings from the file
     /// if both are provided.
     #[arg(long)]
