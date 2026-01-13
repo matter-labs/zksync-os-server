251bb200936eaa20875771cb63833261a46865a4
perekopskiy <mikeson.dp@gmail.com>
2026-01-13 14:06:25 +0200

    fix

--- integration-tests/src/config.rs
+++ integration-tests/src/config.rs
@@ -101,5 +101,7 @@ pub fn get_chain_config(chain_index: usize) -> Config {
         observability_config: Default::default(),
         gas_adjuster_config: Default::default(),
         batch_verification_config: Default::default(),
+        base_token_price_updater_config: config_repo.single().unwrap().parse().unwrap(),
+        external_price_api_client_config: config_repo.single().unwrap().parse().unwrap(),
     }
 }
