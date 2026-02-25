d65bad2951d790e22e8da946fbb44c1ec65c1b45
Lech <88630083+Artemka374@users.noreply.github.com>
2026-02-25 15:33:15 +0100

    fix test

--- lib/batch_verification/src/wire_format/tests/mod.rs
+++ lib/batch_verification/src/wire_format/tests/mod.rs
@@ -31,7 +31,7 @@ fn create_sample_request() -> BatchVerificationRequest {
             last_block_number: Some(2),
             chain_id: 6565,
             operator_da_input: vec![],
-            sl_chain_id: 123,
+            sl_chain_id: 0,
         },
         prev_commit_data: StoredBatchInfo {
             batch_number: 41,
