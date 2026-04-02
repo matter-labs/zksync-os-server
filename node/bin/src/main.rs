b6140db71021e991c2f4ea887e7678e66dec8d65
perekopskiy <mikeson.dp@gmail.com>
2026-04-02 19:45:08 +0200

    Revert "kludge"
    This reverts commit 3f18c94f5cac622793f094cc376785199112c201.

--- node/bin/src/main.rs
+++ node/bin/src/main.rs
@@ -230,33 +230,27 @@ async fn handle_delayed_termination(runtime: Runtime) {
 }
 
 fn enable_ephemeral_mode(config: &mut Config) -> Option<TempDir> {
-    let tempdir = if !config.general_config.semi_ephemeral {
-        let original_path = config.general_config.rocks_db_path.clone();
-        if original_path != Path::new(DEFAULT_ROCKS_DB_PATH) {
-            tracing::warn!(
-                original_path = %original_path.display(),
-                "general_rocks_db_path parameter is ignored in ephemeral mode"
-            );
-        }
-
-        let tempdir = tempfile::tempdir()
-            .expect("Failed to create temporary RocksDB directory for ephemeral mode");
-        let tempdir_path = tempdir.path();
-        tracing::info!(
-            path = %tempdir_path.display(),
-            "Ephemeral mode enabled. Using temporary directory for RocksDB and proof storage"
+    let original_path = config.general_config.rocks_db_path.clone();
+    if original_path != Path::new(DEFAULT_ROCKS_DB_PATH) {
+        tracing::warn!(
+            original_path = %original_path.display(),
+            "general_rocks_db_path parameter is ignored in ephemeral mode"
         );
+    }
 
-        // Update config to use temporary directory
-        config.general_config.rocks_db_path = tempdir_path.join("node");
-        config.prover_api_config.proof_storage = ProofStorageConfig {
-            path: tempdir_path.join("fri_proofs"),
-            ..ProofStorageConfig::default()
-        };
+    let tempdir = tempfile::tempdir()
+        .expect("Failed to create temporary RocksDB directory for ephemeral mode");
+    let tempdir_path = tempdir.path();
+    tracing::info!(
+        path = %tempdir_path.display(),
+        "Ephemeral mode enabled. Using temporary directory for RocksDB and proof storage"
+    );
 
-        Some(tempdir)
-    } else {
-        None
+    // Update config to use temporary directory
+    config.general_config.rocks_db_path = tempdir_path.join("node");
+    config.prover_api_config.proof_storage = ProofStorageConfig {
+        path: tempdir_path.join("fri_proofs"),
+        ..ProofStorageConfig::default()
     };
 
     // Disable services that are not needed in ephemeral mode
@@ -273,7 +267,7 @@ fn enable_ephemeral_mode(config: &mut Config) -> Option<TempDir> {
         );
     }
 
-    tempdir
+    Some(tempdir)
 }
 
 fn load_internal_config(config: &mut Config) {
