4d58eff3efe25e5a1a9517fbd7e52bbdc27013ec
Lech <88630083+Artemka374@users.noreply.github.com>
2026-04-15 14:15:08 +0200

    add interval mining to anvil

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -1159,6 +1159,7 @@ impl AnvilL1 {
                 .chain_id(L1_CHAIN_ID)
                 .arg("--load-state")
                 .arg(l1_state_path)
+                .block_time(1)
         })?;
 
         let wallet = provider.wallet().clone();
