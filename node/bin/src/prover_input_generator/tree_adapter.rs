b828328bbf8523337dd148f14938ea6fc530dd6c
Alex Ostrovski <aov@matterlabs.dev>
2026-04-30 15:32:45 +0300

    Simplify `TreeOutputAdapter`

--- node/bin/src/prover_input_generator/tree_adapter.rs
+++ node/bin/src/prover_input_generator/tree_adapter.rs
@@ -1,5 +1,5 @@
 use alloy::primitives::B256;
-use std::collections::{BTreeMap, HashMap};
+use std::collections::{BTreeMap, HashMap, HashSet};
 use std::thread;
 use zk_ee::utils::Bytes32;
 use zk_os_basic_system::system_implementation::flat_storage_model::FlatStorageLeaf;
@@ -23,31 +23,48 @@ struct NoData;
 pub(super) struct TreeOutputAdapter {
     leaf_count_before_update: u64,
     sorted_leaves: BTreeMap<u64, Leaf>,
-    key_to_index: HashMap<B256, u64>,
-    missing_key_to_prev_index: HashMap<B256, u64>,
+    key_to_op: HashMap<B256, TreeOperation>,
     sibling_hashes: HashMap<(u8, u64), B256>,
-    // TODO: stats on actually queried keys
+    // Fields below are only used for metrics and can be removed in the future.
+    queried_proofs: HashSet<u64>,
+}
+
+impl Drop for TreeOutputAdapter {
+    fn drop(&mut self) {
+        const DEBUG_COUNT: usize = 5;
+
+        if thread::panicking() {
+            return; // Do not report potentially incomplete data if generating prover input failed
+        }
+
+        let expected_queried_proofs: HashSet<_> = self.sorted_leaves.keys().copied().collect();
+        let unexpected_queried_proofs: Vec<_> = self
+            .queried_proofs
+            .difference(&expected_queried_proofs)
+            .take(DEBUG_COUNT)
+            .collect();
+        if !unexpected_queried_proofs.is_empty() {
+            tracing::warn!(
+                ?unexpected_queried_proofs,
+                "prover queried unexpected Merkle proofs"
+            );
+        }
+        let not_queried_proofs: Vec<_> = expected_queried_proofs
+            .difference(&self.queried_proofs)
+            .take(DEBUG_COUNT)
+            .collect();
+        if !not_queried_proofs.is_empty() {
+            tracing::warn!(
+                ?not_queried_proofs,
+                "prover didn't query expected Merkle proofs"
+            );
+        }
+    }
 }
 
 impl TreeOutputAdapter {
     pub(super) fn new(tree_data: BlockMerkleTreeData) -> Self {
-        let key_to_index = tree_data
-            .proof
-            .sorted_leaves
-            .iter()
-            .map(|(index, leaf)| (leaf.key.0.into(), *index))
-            .collect();
-
-        let missing_key_to_prev_index = tree_data
-            .keys_and_ops()
-            .filter_map(|(key, op)| {
-                let TreeOperation::Miss { prev_index } = op else {
-                    return None;
-                };
-                Some((key, prev_index))
-            })
-            .collect();
-
+        let key_to_index: HashMap<_, _> = tree_data.keys_and_ops().collect();
         let sibling_hashes = tree_data
             .proof
             .sibling_hashes(TREE_DEPTH, tree_data.input.leaf_count)
@@ -55,10 +72,10 @@ impl TreeOutputAdapter {
             .collect();
 
         Self {
+            queried_proofs: HashSet::with_capacity(tree_data.proof.sorted_leaves.len()),
             leaf_count_before_update: tree_data.input.leaf_count,
             sorted_leaves: tree_data.proof.sorted_leaves,
-            key_to_index,
-            missing_key_to_prev_index,
+            key_to_op: key_to_index,
             sibling_hashes,
         }
     }
@@ -97,27 +114,27 @@ impl TreeOutputAdapter {
     }
 
     fn read(&mut self, key: B256) -> Result<Option<B256>, NoData> {
-        Ok(if let Some(idx) = self.key_to_index.get(&key) {
-            let leaf = self.sorted_leaves.get(idx).ok_or(NoData)?;
-            Some(leaf.value)
-        } else if self.missing_key_to_prev_index.contains_key(&key) {
-            None
-        } else {
-            return Err(NoData);
+        let op = self.key_to_op.get(&key).ok_or(NoData)?;
+        Ok(match op {
+            TreeOperation::Hit { index } => {
+                let leaf = self.sorted_leaves.get(index).ok_or(NoData)?;
+                Some(leaf.value)
+            }
+            TreeOperation::Miss { .. } => None,
         })
     }
 
     fn tree_index(&mut self, key: B256) -> Result<Option<u64>, NoData> {
-        if let Some(idx) = self.key_to_index.get(&key) {
-            Ok(Some(*idx))
-        } else if self.missing_key_to_prev_index.contains_key(&key) {
-            Ok(None)
-        } else {
-            Err(NoData)
-        }
+        let op = self.key_to_op.get(&key).ok_or(NoData)?;
+        Ok(match op {
+            TreeOperation::Hit { index } => Some(*index),
+            TreeOperation::Miss { .. } => None,
+        })
     }
 
     fn merkle_proof(&mut self, tree_index: u64) -> Result<LeafProof, NoData> {
+        self.queried_proofs.insert(tree_index);
+
         let leaf = self.sorted_leaves.get(&tree_index).ok_or(NoData)?;
         let leaf = FlatStorageLeaf {
             key: leaf.key.0.into(),
@@ -129,10 +146,12 @@ impl TreeOutputAdapter {
     }
 
     fn prev_tree_index(&mut self, key: B256) -> Result<u64, NoData> {
-        self.missing_key_to_prev_index
-            .get(&key)
-            .copied()
-            .ok_or(NoData)
+        let op = self.key_to_op.get(&key).ok_or(NoData)?;
+        if let TreeOperation::Miss { prev_index } = op {
+            Ok(*prev_index)
+        } else {
+            Err(NoData)
+        }
     }
 }
 
