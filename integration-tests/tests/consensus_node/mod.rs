2f2aefbf61c6c26330fc55a835e9df54c34cd163
romanbrodetski <rb@matterlabs.dev>
2026-05-19 09:40:18 +0100

    test(consensus): ignore flaky integration tests

--- integration-tests/tests/consensus_node/mod.rs
+++ integration-tests/tests/consensus_node/mod.rs
@@ -253,6 +253,7 @@ async fn consensus_cluster_rotates_leader_after_failure() -> anyhow::Result<()> 
 }
 
 #[test_log::test(tokio::test)]
+#[ignore = "flaky; @romanbrodetski is working on it"]
 async fn consensus_cluster_stops_making_progress_without_quorum() -> anyhow::Result<()> {
     let mut cluster = MultiNodeTester::builder()
         .with_consensus_secret_keys(consensus_test_keys(3))
@@ -287,6 +288,7 @@ async fn consensus_cluster_stops_making_progress_without_quorum() -> anyhow::Res
 }
 
 #[test_log::test(tokio::test)]
+#[ignore = "flaky; @romanbrodetski is working on it"]
 async fn consensus_original_leader_rejoins_and_cluster_remains_stable() -> anyhow::Result<()> {
     let mut cluster = MultiNodeTester::builder()
         .with_consensus_secret_keys(consensus_test_keys(3))
