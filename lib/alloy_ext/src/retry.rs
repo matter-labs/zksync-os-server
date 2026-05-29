6e374c7b764d59e55bdacf360eb2e2c2854555fe
Lech <88630083+Artemka374@users.noreply.github.com>
2026-05-29 17:24:13 +0200

    add a wrapper for scoped retry override

--- lib/alloy_ext/src/retry.rs
+++ lib/alloy_ext/src/retry.rs
@@ -82,7 +82,7 @@ tokio::task_local! {
 ///
 /// This is useful for higher-level provider calls that do not expose the underlying
 /// `RpcCall` where [`RpcRetryExt`] can attach metadata directly.
-pub async fn with_rpc_retry_override<F>(override_: RpcRetryOverride, future: F) -> F::Output
+pub async fn with_scoped_rpc_retry_override<F>(override_: RpcRetryOverride, future: F) -> F::Output
 where
     F: Future,
 {
@@ -93,22 +93,6 @@ where
 pub fn scoped_rpc_retry_override() -> Option<RpcRetryOverride> {
     SCOPED_RPC_RETRY_OVERRIDE.try_with(Clone::clone).ok()
 }
-
-/// Adds scoped retry metadata to provider futures that hide their underlying RPC call.
-pub trait RpcRetryFutureExt: Future + Sized {
-    fn with_retry_override(
-        self,
-        override_: RpcRetryOverride,
-    ) -> impl Future<Output = Self::Output> {
-        with_rpc_retry_override(override_, self)
-    }
-
-    fn with_infinite_retries(self, call_name: &'static str) -> impl Future<Output = Self::Output> {
-        self.with_retry_override(RpcRetryOverride::infinite(call_name))
-    }
-}
-
-impl<F> RpcRetryFutureExt for F where F: Future {}
 
 /// Adds per-call retry metadata to prepared Alloy RPC calls.
 pub trait RpcRetryExt: Sized {
@@ -140,7 +124,7 @@ where
 {
     fn with_retry_override(self, override_: RpcRetryOverride) -> Self {
         match self {
-            Self::RpcCall(call) => Self::RpcCall(RpcRetryExt::with_retry_override(call, override_)),
+            Self::RpcCall(call) => Self::RpcCall(call.with_retry_override(override_)),
             other => other,
         }
     }
