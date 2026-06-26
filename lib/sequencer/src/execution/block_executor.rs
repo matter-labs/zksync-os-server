47a4d2cc157d72226fcf092c484d2ad6527f668d
Daniyar Itegulov <ditegulov@gmail.com>
2026-06-26 13:50:35 +1000

    feat(sequencer): disable `wait_for_block_applier`

--- lib/sequencer/src/execution/block_executor.rs
+++ lib/sequencer/src/execution/block_executor.rs
@@ -80,14 +80,14 @@ where
             tracing::info!("Command {cmd} received by BlockExecutor");
             let cmd_type = cmd.command_type();
             state_reporter.enter_state(SequencerState::WaitingForApplier);
-            // If we had a non-genesis block in the past, we need to wait for the block applier to
-            // catch up. Genesis is skipped because it never gets applied further down the pipeline.
-            if let Some(last_block_number) = self.block_context_provider.last_block_number()
-                && last_block_number > 0
-            {
-                wait_for_block_applier(&mut self.applied_block_number_receiver, last_block_number)
-                    .await?;
-            }
+            // // If we had a non-genesis block in the past, we need to wait for the block applier to
+            // // catch up. Genesis is skipped because it never gets applied further down the pipeline.
+            // if let Some(last_block_number) = self.block_context_provider.last_block_number()
+            //     && last_block_number > 0
+            // {
+            //     wait_for_block_applier(&mut self.applied_block_number_receiver, last_block_number)
+            //         .await?;
+            // }
 
             // For Produce commands: check limit (will await indefinitely if limit reached) and increment counter
             if matches!(cmd, BlockCommand::Produce(_))
@@ -226,39 +226,39 @@ where
     }
 }
 
-async fn wait_for_block_applier(
-    applied_block_number_receiver: &mut watch::Receiver<Option<BlockNumber>>,
-    required_block_number: BlockNumber,
-) -> anyhow::Result<()> {
-    let applied_block_number = *applied_block_number_receiver.borrow_and_update();
-    if applied_block_number >= Some(required_block_number) {
-        tracing::debug!(
-            applied_block_number,
-            required_block_number,
-            "BlockExecutor does not need to wait for BlockApplier"
-        );
-        return Ok(());
-    }
-
-    tracing::debug!(
-        applied_block_number,
-        required_block_number,
-        "BlockExecutor waiting for BlockApplier to catch up"
-    );
-
-    let reached_block_number = applied_block_number_receiver
-        .wait_for(|block_number| *block_number >= Some(required_block_number))
-        .await
-        .context("block applier progress watch closed while executor was waiting")?
-        .to_owned();
-
-    tracing::debug!(
-        reached_block_number,
-        required_block_number,
-        "BlockExecutor resumed after BlockApplier caught up"
-    );
-    Ok(())
-}
+// async fn wait_for_block_applier(
+//     applied_block_number_receiver: &mut watch::Receiver<Option<BlockNumber>>,
+//     required_block_number: BlockNumber,
+// ) -> anyhow::Result<()> {
+//     let applied_block_number = *applied_block_number_receiver.borrow_and_update();
+//     if applied_block_number >= Some(required_block_number) {
+//         tracing::debug!(
+//             applied_block_number,
+//             required_block_number,
+//             "BlockExecutor does not need to wait for BlockApplier"
+//         );
+//         return Ok(());
+//     }
+//
+//     tracing::debug!(
+//         applied_block_number,
+//         required_block_number,
+//         "BlockExecutor waiting for BlockApplier to catch up"
+//     );
+//
+//     let reached_block_number = applied_block_number_receiver
+//         .wait_for(|block_number| *block_number >= Some(required_block_number))
+//         .await
+//         .context("block applier progress watch closed while executor was waiting")?
+//         .to_owned();
+//
+//     tracing::debug!(
+//         reached_block_number,
+//         required_block_number,
+//         "BlockExecutor resumed after BlockApplier caught up"
+//     );
+//     Ok(())
+// }
 
 /// Checks if block production limit has been reached.
 /// If limit is reached, signals to stop accepting transactions and awaits indefinitely (never returns).
